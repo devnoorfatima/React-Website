954fb9cd6985ee31fd79df7d81af3f3471aa665e
Noor Fatima <devnoorfatima@gmail.com>
2019-09-08 22:10:02 +0500

    Making a react ebsite project.

--- website-app/src/Components/work.js
+++ website-app/src/Components/work.js
@@ -3,48 +3,57 @@ import {Card} from 'react-bootstrap';
 function Work() {
   return (
     <div className="work">
-     <h3 className='text-center'>Work we've Done So Far</h3>
-    <div className='row mx-auto'>
-      <div className='col-sm-3 ml-4'>
-      <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+     <h3 className='text-center m-5'>Work we've Done So Far</h3>
+  <div className='row mx-auto'>
+  <div className='col-sm-3 ml-4'>
+  <Card style={{ width: '19rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1496093044462-e7ee398276c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
+    <Card.Title className='text-center'>Card Title</Card.Title>
   </Card.Body>
 </Card>
 </div>
 <div className='col-sm-3 mx-auto'>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="../1.jpeg" />
+<Card style={{ width: '19rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1562184552-997c461abbe6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
+    <Card.Title className='text-center'>Card Title</Card.Title>
   </Card.Body>
 </Card>
 </div>
-<div className='col-sm-3 ml-auto'>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+<div className='col-sm-3 ml-auto mr-4'>
+<Card style={{ width: '19rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1553524810-47c06fe4462c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
+    <Card.Title className='text-center'>Card Title</Card.Title>
+  </Card.Body>
+</Card>
+      </div>
+      <div className='col-sm-3 mt-5 ml-4'>
+  <Card style={{ width: '19rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1542372533-4b345928c0a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80" />
+  <Card.Body>
+    <Card.Title className='text-center'>Card Title</Card.Title>
+  </Card.Body>
+</Card>
+</div>
+<div className='col-sm-3 mt-5 mx-auto'>
+<Card style={{ width: '19rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1556228453-efd6c1ff04f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+  <Card.Body>
+    <Card.Title className='text-center'>Card Title</Card.Title>
+  </Card.Body>
+</Card>
+</div>
+<div className='col-sm-3 mt-5 mr-4 ml-auto'>
+<Card style={{ width: '19rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1505692433770-36f19f51681d?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+  <Card.Body>
+    <Card.Title className='text-center'>Card Title</Card.Title>
   </Card.Body>
 </Card>
       </div>
     </div>
-
-
-
     </div>
   );
 }
