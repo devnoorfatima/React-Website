e9a0db67a07c6d0c0206793fdba15c7738b02131
Noor Fatima <devnoorfatima@gmail.com>
2019-09-17 20:47:02 +0500

    Styling the website.

--- website-app/src/Components/contact.js
+++ website-app/src/Components/contact.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import "../styles/contact.css";
 function Contact() {
   return (
-    <div className="Contact">
-   hello
+    <div className="Contact text-center">
+     <h2 className='text-center pt-5 mb-4'>Contact Us</h2>
+     <h5 className='text-center mb-4'>Our Passion is to satisfy our clients</h5>
+     <label>Name:</label>
+     <input type='text' className='m-3' />
+     <label>Contact No:</label>
+     <input type='number' className='m-3' />
+     <div className='message mt-4'>
+     <p class="formfield">
+  <label for="textarea" >Your Message : </label>
+  <textarea className='m-3' id="textarea" placeholder='Your message here...' ></textarea>
+</p>
+      </div>
+     
     </div>
   );
 }
