b1ba739a68bae09516efe9923e20f57958d36119
Noor Fatima <devnoorfatima@gmail.com>
2019-09-26 22:19:59 +0500

    Changing contacts part

--- website-app/src/Components/contact.js
+++ website-app/src/Components/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "../styles/contact.css";
 function Contact() {
   return (
-    <div className="Contact text-center">
+  <div className="Contact text-center">
      <h2 className='text-center pt-5 mb-4'>Contact Us</h2>
      <h5 className='text-center mb-4'>Our Passion is to satisfy our clients</h5>
      <label>Name:</label>
@@ -10,13 +10,10 @@ function Contact() {
      <label>Contact No:</label>
      <input type='number' className='m-3' />
      <div className='message mt-4'>
-     <p class="formfield">
-  <label for="textarea" >Your Message : </label>
-  <textarea className='m-3' id="textarea" placeholder='Your message here...' ></textarea>
-</p>
-      </div>
+     <textarea className='m-3' id="textarea" placeholder='Your message here...' ></textarea>
+     </div>
      
-    </div>
+   </div>
   );
 }
 
